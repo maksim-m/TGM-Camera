a50a6ec840279840bf40c97bba03fd741074b46b
Maksim Moiseikin <m.moiseikin@gmail.com>
2016-04-28 20:16:30 +0200

    initial commit.

---
+++ app/src/main/java/me/maxdev/tgmcamera/CameraActivity.java
@@ -0,0 +1,98 @@
+package me.maxdev.tgmcamera;
+
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CameraActivity extends AppCompatActivity implements View.OnSystemUiVisibilityChangeListener {
+
+    private static final String LOG_TAG = "CameraActivity";
+
+    @BindView(R.id.root_layout)
+    FrameLayout rootLayout;
+    @BindView(R.id.preview_layout)
+    FrameLayout previewLayout;
+
+    private Camera camera;
+    private CameraPreview cameraPreview;
+
+    private Runnable systemUiHider = new Runnable() {
+        @Override
+        public void run() {
+            Log.d(LOG_TAG, "Start systemUiHider.");
+            hideStatusUi();
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        ButterKnife.bind(this);
+        rootLayout.setOnSystemUiVisibilityChangeListener(this);
+        hideStatusUi();
+        initCameraPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // TODO releaseMediaRecorder();
+        releaseCamera();
+    }
+
+    private void initCameraPreview() {
+        camera = getCameraInstance();
+        if (camera == null) {
+            // TODO: show error dialog
+        } else {
+            cameraPreview = new CameraPreview(this, camera);
+            //cameraPreview.updateCameraParams(camera);
+            previewLayout.addView(cameraPreview);
+        }
+    }
+
+    private void releaseCamera() {
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+
+    }
+
+    private void hideStatusUi() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(0); // reset all flags
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LOW_PROFILE |
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
+
+    @Override
+    public void onSystemUiVisibilityChange(int visibility) {
+        rootLayout.getHandler().postDelayed(systemUiHider, 2000);
+    }
+
+    public static Camera getCameraInstance() {
+        Camera camera = null;
+        try {
+            camera = Camera.open(); // attempt to get a Camera instance
+        } catch (Exception e) {
+            // Camera is not available (in use or does not exist)
+            // TODO
+        }
+        return camera;
+    }
+
+}
---
+++ app/src/main/java/me/maxdev/tgmcamera/CameraPreview.java
@@ -0,0 +1,144 @@
+package me.maxdev.tgmcamera;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by Max on 28.04.2016.
+ */
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+
+    private static final String LOG_TAG = "CameraPreview";
+    private Camera camera;
+    private SurfaceHolder surfaceHolder;
+    private List<Camera.Size> supportedPreviewSizes;
+    private Camera.Size previewSize;
+
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
+        this.surfaceHolder = getHolder();
+        this.surfaceHolder.addCallback(this);
+        if (camera != null) {
+            supportedPreviewSizes = camera.getParameters().getSupportedPreviewSizes();
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, now tell the camera where to draw the preview.
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        } catch (IOException e) {
+            Log.d(LOG_TAG, "Error setting camera preview: " + e.getMessage());
+        }
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+
+        if (surfaceHolder.getSurface() == null) {
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+            camera.stopPreview();
+        } catch (Exception e) {
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // TODO: set preview size and make any resize, rotate or reformatting changes here
+        /*
+        If you want to set a specific size for your camera preview, set this in the surfaceChanged()
+        method as noted in the comments above. When setting preview size, you must use values
+        from getSupportedPreviewSizes(). Do not set arbitrary values in the setPreviewSize() method.
+         */
+        if (camera != null) {
+            Camera.Parameters parameters = camera.getParameters();
+            parameters.setPreviewSize(previewSize.width, previewSize.height);
+            requestLayout();
+            camera.setParameters(parameters);
+        }
+
+        // start preview with new settings
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+
+        } catch (Exception e) {
+            Log.d(LOG_TAG, "Error starting camera preview: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // empty. Take care of releasing the Camera preview in your activity.
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        final int width = resolveSize(getSuggestedMinimumWidth(), widthMeasureSpec);
+        final int height = resolveSize(getSuggestedMinimumHeight(), heightMeasureSpec);
+        setMeasuredDimension(width, height);
+
+        if (supportedPreviewSizes != null) {
+            previewSize = getOptimalPreviewSize(supportedPreviewSizes, width, height);
+        }
+    }
+
+    public void updateCameraParams(Camera camera) {
+        if (camera != null) {
+            Camera.Parameters params = camera.getParameters();
+            params.setPreviewSize(previewSize.width, previewSize.height);
+            camera.setParameters(params);
+        }
+    }
+
+    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.1;
+        double targetRatio = (double) h / w;
+
+        if (sizes == null) {
+            return null;
+        }
+
+        Camera.Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        for (Camera.Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) {
+                continue;
+            }
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
+    }
+}
